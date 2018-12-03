94c2c260bdf627a4e1bdc4e162d2746bfba23997
Richard Long <richard.long.e@gmail.com>
2018-12-03 13:07:37 -0500

    update Users to have a login route and also to use the hook beforesave from sequiliZE.

--- app.js
+++ app.js
@@ -3,12 +3,32 @@ require('./config/config');
 const models = require('./models');
 require('./global_functions');
 const sessions = require('./controllers/SessionsController');
-const users = require('./controllers/UsersController');
+const userController  = require('./controllers/UsersController');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const Users = require('./models').Users;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+var opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = CONFIG.jwt_encryption;
+
+passport.use(new JwtStrategy(opts, async function (jwt_payload, done) {
+  let err, user;
+  [err, user] = await to(Users.findById(jwt_payload.user_id));
+
+  if (err) return done(err, false);
+  if (user) {
+    return done(null, user);
+  } else {
+    return done(null, false);
+  }
+}));
 
 // CORS
 app.use(function (req, res, next) {
@@ -41,16 +61,17 @@ if (CONFIG.app === 'dev') {
   models.sequelize.sync();
 }
 
-
 app.get('/sessions', sessions.getAll);
 app.get('/sessions/:sessionId', sessions.get);
 app.post('/sessions', sessions.create);
 app.put('/sessions', sessions.update);
 
+app.post('/users', userController.create);
+app.post('/login', userController.login);
 
-app.get('/users',users.getAll);
-app.get('/users/:currentuserId', users.get);
-app.post('/users', users.create);
-app.put('/users', users.update);
-app.delete('/users', users.deleteuser);
+app.get('/users',userController.getAll);
+app.get('/users/:currentuserId', userController.get);
+app.put('/users', userController.update);
+app.delete('/users', userController.deleteuser);
+
 module.exports = app;
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,66 @@
 const Users = require('../models').Users;
+const validator = require('validator');
+
+const create = async function (req, res) {
+  res.setHeader('ContentType', 'application/json');
+  const body = req.body;
+
+  if (!body.email) {
+    return ReE(res, 'Please enter an email to register', 422);
+  } else if (!body.password) {
+    return ReE(res, 'Please enter a password to register', 422);
+  } else {
+    let err, user
+
+    [err, user] = await to(createUser(body));
+    if (err) return ReE(res, err, 422);
+
+    return ReS(res, user, 201);
+  }
+}
+module.exports.create = create;
+
+const createUser = async function (userInfo) {
+  let err;
+  if (validator.isEmail(userInfo.email)) {
+    [err, user] = await to(Users.create(userInfo));
+    if (err) TE('User already exists with that email');
+    return user;
+  } else {
+    TE('Email is invalid');
+  }
+}
+module.exports.createUser = createUser;
+
+const login = async function (req, res) {
+  const body = req.body;
+  let err, user;
+
+  [err, user] = await to(authUser(req.body));
+  if (err) return ReE(res, err, 422);
+    return ReS(res, { token: user.getJWT(), user: user.toJSON() });
+}
+module.exports.login = login;
+
+const authUser = async function (userInfo) {//returns token
+  if (!userInfo.email) TE('Please enter an email to login');
+  if (!userInfo.password) TE('Please enter a password to login');
+  let user;
+  if (validator.isEmail(userInfo.email)) {
+    [err, user] = await to(Users.findOne({ where: { email: userInfo.email } }));
+    if (err) TE(err.message);
+  } else {
+    TE('A valid email was not entered');
+  }
+
+  if (!user) TE('Not registered');
+    [err, user] = await to(user.comparePassword(userInfo.password));
+
+  if (err) TE(err.message);
+    return user;
+
+}
+module.exports.authUser = authUser;
 
 const getAll = async (req, res) => {
   res.setHeader('Content-Type', 'application/json');
@@ -56,63 +118,3 @@ const update = async function (req, res) {
 }
 
 module.exports.update = update;
-
-const create = async function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  let err, currentuser, currentuserInfo;
-
-  currentuserInfo = req.body;
-
-  [err, currentuser] = await to(Users.create(currentuserInfo));
-  console.log(req.body)
-  console.log('currentuserInfo');
-  if (err) {
-    if (typeof err == 'object' && typeof err.message != 'undefined') {
-      err = err.message;
-    }
-
-    if (typeof code !== 'undefined') res.statusCode = code;
-    res.statusCode = 422; // unprocessable entity
-    return res.json({ success: false, error: err });
-  }
-  [err, currentuser] = await to(currentuser.save());
-  if (err) {
-    if (typeof err == 'object' && typeof err.message != 'undefined') {
-      err = err.message;
-    }
-
-    if (typeof code !== 'undefined') res.statusCode = code;
-    res.statusCode = 422
-    return res.json({ success: false, error: err });
-
-  }
-  res.statusCode = 201;
-  return res.json(currentuser);
-}
-
-module.exports.create = create;
-
-const deleteuser = async function (req, res) {
-  let err, user, data;
-  data = req.body;
-  console.log (req.body);
-  [err, user] = await to(Users.destroy(data, {
-    where: {
-      id: data.id
-    },
-    paranoid 
-  }));
-  if (err) {
-    if (typeof err == 'object' && typeof err.message != 'undefined') {
-      err = err.message;
-    }
-
-    if (typeof code !== 'undefined') res.statusCode = code;
-    res.statusCode = 422
-    return res.json({ success: false, error: err });
-  }
-  return res.json(user);
-}
-
-module.exports.deleteuser = deleteuser;
-
